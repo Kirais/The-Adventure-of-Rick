f3474d3b12964f49d2ab0aa3f2b4629ee9d8a294
Huang Junye <h.jun.ye@gmail.com>
2019-08-21 23:07:35 +0800

    Correct the size of world map

---
+++ assets/backgrounds/World Map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="World Map" tilewidth="8" tileheight="8" tilecount="320" columns="20">
+ <image source="World Map.png" width="160" height="128"/>
+</tileset>
