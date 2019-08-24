ee05e146cb1450039c7ecb274f3e2dd16a1cef79
Huang Junye <h.jun.ye@gmail.com>
2019-08-24 20:03:12 +0800

    Redesign menu

---
+++ assets/backgrounds/MenuIcons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="MenuIcons" tilewidth="8" tileheight="8" tilecount="16" columns="8">
+ <image source="../sprites/MenuIcons.png" width="64" height="16"/>
+</tileset>
---
+++ assets/backgrounds/papers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="papers" tilewidth="8" tileheight="8" tilecount="8" columns="4">
+ <image source="../sprites/papers.png" width="32" height="16"/>
+</tileset>
