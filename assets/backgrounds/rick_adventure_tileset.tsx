577315957b21ac373173b79dafc811398b4a5500
Huang Junye <h.jun.ye@gmail.com>
2019-08-25 13:26:56 +0800

    Add new home sprites

---
+++ assets/backgrounds/rick_adventure_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="rick_adventure_tileset" tilewidth="8" tileheight="8" tilecount="1600" columns="40">
+ <image source="rick_adventure_tileset.png" width="320" height="320"/>
+</tileset>
